6fe120bde8d5a40ccd95e55ff26eb3c0e030a51a
rumza <rummansamsudeen@gmail.com>
2012-11-14 00:02:20 -0500

    Update base/qt4/nativestyle.cpp

--- base/qt4/nativestyle.cpp
+++ base/qt4/nativestyle.cpp
@@ -58,3 +58,148 @@ void NativeStyle::drawPushButton(const StyleFeatures &features, QPainter *painte
         linearGrad.setColorAt(1, QColor(35, 112, 189));
         linearGrad.setColorAt(0, QColor(50, 139, 222));
         //QColor(189, 191, 196)
+        painter->fillPath(backgroundPath, linearGrad);
+        QPen pen(QColor(56, 86, 122), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+        painter->setPen(pen);
+        painter->drawPath(backgroundPath);
+    }
+}
+
+
+
+
+void NativeStyle::drawWindowButton(const StyleFeatures &features, QPainter *painter)
+{
+    QRectF rect = features.exposeRect;
+
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+
+    QPainterPath backgroundPath;
+    backgroundPath.addRoundedRect(rect, 3.5, 3.5);
+
+    QLinearGradient linearGrad(QPointF(0, 0), QPointF(0.0 , rect.height()));
+
+    if(features.state == StyleFeatures::SF_Raised) {
+        linearGrad.setColorAt(0, QColor(35, 112, 189));
+        linearGrad.setColorAt(1, QColor(50, 139, 222));
+        //QColor(189, 191, 196)
+        painter->fillPath(backgroundPath, linearGrad);
+        QPen pen(QColor(56, 86, 122), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+        painter->setPen(pen);
+        painter->drawPath(backgroundPath);
+    }
+    else {
+        linearGrad.setColorAt(1, QColor(35, 112, 189));
+        linearGrad.setColorAt(0, QColor(50, 139, 222));
+        //QColor(189, 191, 196)
+        painter->fillPath(backgroundPath, linearGrad);
+        QPen pen(QColor(56, 86, 122), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+        painter->setPen(pen);
+        painter->drawPath(backgroundPath);
+    }
+}
+
+
+
+
+void NativeStyle::drawFrame(const StyleFeatures &features, QPainter *painter)
+{
+    QRectF rect = features.exposeRect;
+
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+
+    QPainterPath backgroundPath;
+    backgroundPath.addRoundedRect(rect, 4, 4);
+
+    QLinearGradient linearGrad(QPointF(0, 0), QPointF(0.0 , rect.height()));
+
+    linearGrad.setColorAt(1, QColor(189, 191, 196));
+    linearGrad.setColorAt(0, QColor(255, 255, 255));
+    //QColor(189, 191, 196)
+    painter->fillPath(backgroundPath, linearGrad);
+    QPen pen(QColor(98, 101, 108), 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    painter->setPen(pen);
+    painter->drawPath(backgroundPath);
+}
+
+void NativeStyle::drawPushButtonText(const StyleFeatures &features, const QString &text, QPainter *painter)
+{
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+    QPen pen(QColor(255, 255, 255), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    painter->setPen(QColor(255, 255, 255));
+    painter->drawText(features.exposeRect, Qt::AlignCenter, text);
+}
+
+
+void NativeStyle::drawWindowButtonText(const StyleFeatures &features, const QString &text, QPainter *painter)
+{
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+    QPen pen(QColor(255, 255, 255), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    painter->setPen(QColor(255, 255, 255));
+    painter->drawText(features.exposeRect, Qt::AlignCenter, text);
+}
+
+
+void NativeStyle::drawLineEdit(const StyleFeatures &features, QPainter *painter)
+{
+    QRectF rect = features.exposeRect;
+
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+
+    QPainterPath backgroundPath;
+    backgroundPath.addRoundedRect(rect, 2, 2);
+
+    QLinearGradient linearGrad(QPointF(0, 0), QPointF(0.0 , rect.height()));
+
+    linearGrad.setColorAt(0, QColor(189, 191, 196));
+    linearGrad.setColorAt(1, QColor(255, 255, 255));
+
+    painter->fillPath(backgroundPath, linearGrad);
+    QPen pen;
+    if(features.state == StyleFeatures::SF_MouseOver) {
+        pen = QPen(QColor(98, 101, 208), 2, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    } else {
+        pen = QPen(QColor(98, 101, 108), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    }
+    painter->setPen(pen);
+    painter->drawPath(backgroundPath);
+}
+
+
+
+void NativeStyle::drawLineEditText(const StyleFeatures &features, const QString &text, QPainter *painter)
+{
+    /* Painter settings */
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    painter->setRenderHint(QPainter::TextAntialiasing, true);
+    painter->setRenderHint(QPainter::HighQualityAntialiasing, true);
+    QPen pen(QColor(0, 0, 0), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+
+    QFont font = QFont("", 20);
+    QFontMetrics fontMetrics(font);
+    int width = fontMetrics.width(text.left(features.cursorLocation));
+
+    painter->setFont(font);
+    painter->setPen(pen);
+    painter->drawText(features.exposeRect, Qt::AlignLeft | Qt::AlignVCenter, text);
+    pen = QPen(QColor(98, 101, 108), 1, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
+    painter->setPen(pen);
+    painter->drawLine(QPoint(width, 4), QPoint(width,features.exposeRect.height() - 4));
+}
+
+}
